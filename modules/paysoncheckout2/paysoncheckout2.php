30f651d1d11dd0bf122ac365bc7f2411954bb20b
oscar-villll <oscarsuecia@gmail.com>
2022-10-31 13:48:28 +0100

    3.0.28
    Minor change

--- modules/paysoncheckout2/paysoncheckout2.php
+++ modules/paysoncheckout2/paysoncheckout2.php
@@ -28,7 +28,7 @@ class PaysonCheckout2 extends PaymentModule
     {
         $this->name = 'paysoncheckout2';
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.27';
+        $this->version = '3.0.28';
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
         $this->author = 'Payson AB';
         $this->module_key = '4015ee54469de01eaa9150b76054547e';
